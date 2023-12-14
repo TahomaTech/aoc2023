caec934b61c3c7178d1c665b06b05519958cb133
Ryan Hendrickson <8751750+TahomaTech@users.noreply.github.com>
2023-12-13 19:40:56 -0800

    Add Day01Part02 tests

--- tests/Unit/Day01Test.php
+++ tests/Unit/Day01Test.php
@@ -28,4 +28,25 @@ class Day01Test extends TestCase
         yield 'a1b2c3d4e5f = 15' => ['a1b2c3d4e5f', 15];
         yield 'treb7uchet = 77' => ['treb7uchet', 77];
     }
+
+    #[DataProvider("part02Provider")]
+    public function testDay01Part02SolutionWorks(string $inputString, int $expectedNumber): void
+    {
+        $day01 = new Day01();
+
+        $result = $day01->part02Solution($inputString);
+
+        self::assertSame($result, $expectedNumber);
+    }
+
+    public static function part02Provider(): Generator
+    {
+        yield 'two1nine = 29' => ['two1nine', 29];
+        yield 'eightwothree = 83' => ['eightwothree', 83];
+        yield 'abcone2threexyz = 13' => ['abcone2threexyz', 13];
+        yield 'xtwone3four = 24' => ['xtwone3four', 24];
+        yield '4nineeightseven2 = 42' => ['4nineeightseven2', 42];
+        yield 'zoneight234 = 14' => ['zoneight234', 14];
+        yield '7pqrstsixteen = 76' => ['7pqrstsixteen', 76];
+    }
 }
